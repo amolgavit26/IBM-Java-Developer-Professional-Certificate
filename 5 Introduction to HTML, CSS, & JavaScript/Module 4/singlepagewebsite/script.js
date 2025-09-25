22ad1bf151c31ef368b3cc49c47cfd8ef48db5d6
Amol Gavit <amolgavit158121@gmail.com>
2025-09-26 01:17:02 +0530

    Introduction to HTML, CSS, & JavaScript

---
+++ 5 Introduction to HTML, CSS, & JavaScript/Module 4/singlepagewebsite/script.js
@@ -0,0 +1,30 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+
+    // Create a new 'recommendation' element and set its value
+    var element = document.createElement("div");
+    element.setAttribute("class", "recommendation");
+    element.innerHTML = "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
+
+    // Add to recommendations
+    document.getElementById("all_recommendations").appendChild(element);
+
+    // Reset textarea
+    recommendation.value = "";
+
+    // Show popup
+    showPopup(true);
+  }
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible';
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden';
+  }
+}
